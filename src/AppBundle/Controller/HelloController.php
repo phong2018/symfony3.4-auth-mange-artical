b9f6b289c3f3154b4d3d1a605feddf42169be0c0
phong2018 <phong2018@gmail.com>
2020-10-08 09:19:06 +0700

    bo sung gui mail bang SwiftMailer

--- src/AppBundle/Controller/HelloController.php
+++ src/AppBundle/Controller/HelloController.php
@@ -17,10 +17,45 @@ class HelloController extends Controller
     }
 
     /**
-     * @Route("/admin")
+     * @Route("/admin1")
      */
     public function adminAction()
     {
+        return new Response('<html><body>HELLO ADMIN page!</body></html>');
+    }
+
+    /** 
+   * @Route("/mailsample/send", name="mail_sample_send") 
+    */ 
+    public function MailerSample($name="TEST MAIL", \Swift_Mailer $mailer)
+    {
+        $message = (new \Swift_Message('Hello Email'))
+            ->setFrom('send@example.com')
+            ->setTo('phong2018@gmail.com')
+            ->setBody(
+                $this->renderView(
+                    // app/Resources/views/emails/registration.html.twig
+                    'emails/registration.html.twig',
+                    ['name' => $name]
+                ),
+                'text/html'
+            )
+
+            // you can remove the following code if you don't define a text version for your emails
+            ->addPart(
+                $this->renderView(
+                    'emails/registration.html.twig',
+                    ['name' => $name]
+                ),
+                'text/plain'
+            )
+        ;
+
+        $mailer->send($message);
+
+        // or, you can also fetch the mailer service this way
+        // $this->get('mailer')->send($message);
+
         return new Response('<html><body>HELLO ADMIN page!</body></html>');
     }
 }
